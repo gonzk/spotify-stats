49941b50166cf24d25448b3a355d43cdb3d328a2
Kristine Gonzales <kgonz@student.ubc.ca>
2022-01-29 09:40:35 -0800

    Modified package.json and Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,9 +8,7 @@ import Auth from '../Auth';
 const USER_ENDPOINT = "https://api.spotify.com/v1/me/";
 
 export default function Home() {
-    useEffect(() => {
-        Auth();
-    }, [])
+    Auth();
     const [data, setData] = useState({});
     const token = localStorage.getItem('accessToken');
 
